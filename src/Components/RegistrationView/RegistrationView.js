a4b376652560696e9e6b0e3a7788d914c283ac83
Mateusz Rajek <mateusz_rajek@wp.pl>
2021-05-09 14:05:00 +0100

    Created Button component and implemented in to form; Added field for existing account; Added User Name field in registration form

--- src/Components/RegistrationView/RegistrationView.js
+++ src/Components/RegistrationView/RegistrationView.js
@@ -1,32 +1,36 @@
 import React, { useState } from 'react';
-import { useForm } from "react-hook-form";
+import Button from '../Button/Button';
 import './RegistrationView.sass';
 import Password from './Steps/Password';
 import PersonalData from './Steps/PersonalData';
 
 
 function RegistrationView() {
-const { register, handleSubmit } = useForm()
 const [registrationData, setRegData] = useState({})
 const [registrationStep, setRegStep] = useState(1)
+
+const changeStep = () => {
+  registrationStep === 1 ? setRegStep(2) : setRegStep(1)
+}
 
 const onSubmit = data => {
   let formData = registrationData
   formData = {...formData, ...data}
 
   setRegData(formData)
+  changeStep()
 console.log(formData)
 }
 
   return (
     <>
     <section className='registration__container'>
-        <form className='registration__container__form' onSubmit={handleSubmit(onSubmit)}>
-          <h1 className='registration__form__title'>Create an account</h1>
-          {registrationStep === 1 && <PersonalData register={register}/>}
-          {registrationStep === 2 && <Password register={register} />}
-          <button type='submit'>Click</button>
-        </form>
+        {registrationStep === 1 && <PersonalData onClick={changeStep} onSubmit={onSubmit} formData={registrationData} />}
+        {registrationStep === 2 && <Password onClick={changeStep} onSubmit={onSubmit} />}
+        <div className='registration__form__login'>
+          <p>Already have an account?</p>
+          <Button text='Sign in' size='small' color='lightcollar' />
+        </div>
     </section>
       
     </>

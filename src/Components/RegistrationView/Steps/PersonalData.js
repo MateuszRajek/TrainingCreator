24070f2cf28c860eaed0d7eec8c0daf2f2e92a2e
Mateusz Rajek <mateusz_rajek@wp.pl>
2021-05-12 16:48:06 +0100

    Added phone number field validation

--- src/Components/RegistrationView/Steps/PersonalData.js
+++ src/Components/RegistrationView/Steps/PersonalData.js
@@ -15,7 +15,7 @@ function PersonalData({ onSubmit }) {
         <h1 className='registration__form__title'>Create an account</h1>
         <Input 
           className='registration__form__input' 
-          register={register('firstName', {required: true, minLength: 2})} 
+          register={register('firstName', {required: true, minLength: 2, pattern: /^[\s\p{L}]+$/u})} 
           text='First Name' 
           id='firstName' 
           type='text'
@@ -25,9 +25,11 @@ function PersonalData({ onSubmit }) {
         <p className='registration__form__error-message'>First Name is required</p>}
         {errors.firstName && errors.firstName.type === 'minLength' && 
         <p className='registration__form__error-message'>First Name requires at least 2 characters</p>}
+        {errors.firstName && errors.firstName.type === 'pattern' && 
+        <p className='registration__form__error-message'>Only letters allowed in this field</p>}
         <Input 
           className='registration__form__input' 
-          register={register('lastName', {required: true, minLength: 2})} 
+          register={register('lastName', {required: true, minLength: 2, pattern: /^[\s\p{L}]+$/u})} 
           text='Last Name' 
           id='lastName' 
           type='text'
@@ -37,6 +39,8 @@ function PersonalData({ onSubmit }) {
         <p className='registration__form__error-message'>Last Name is required</p>}
         {errors.lastName && errors.lastName.type === 'minLength' && 
         <p className='registration__form__error-message'>Last Name requires at least 2 characters</p>}
+        {errors.lastName && errors.lastName.type === 'pattern' && 
+        <p className='registration__form__error-message'>Only letters allowed in this field</p>}
         <Input 
           className='registration__form__input' 
           register={register('email', {required: true, pattern: /^[\w-a\\.]+@([\w-]+\.)+[\w-]{2,4}$/g})} 
@@ -51,12 +55,18 @@ function PersonalData({ onSubmit }) {
         <p className='registration__form__error-message'>Invalid email format</p>}
         <Input 
           className='registration__form__input' 
-          register={register('phone', {required: true, minLength: 6})} 
+          register={register('phone', {required: true, minLength: 6, pattern: /^[0-9]*$/})} 
           text='Phone Number' 
           id='phone' 
           type='phone'
           icon={phone} 
         />
+        {errors.phone && errors.phone.type === 'required' && 
+        <p className='registration__form__error-message'>Phone number is required</p>}
+        {errors.phone && errors.phone.type === 'minLength' && 
+        <p className='registration__form__error-message'>Phone number requires at least 6 characters</p>}
+        {errors.phone && errors.phone.type === 'pattern' && 
+        <p className='registration__form__error-message'>Only numbers allowed</p>}
         <div className='registration__form__buttons'>
           <Button text='Next' size='small' color='aquamarine' />
         </div>

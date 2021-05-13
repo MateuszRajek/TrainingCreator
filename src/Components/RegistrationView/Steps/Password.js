686ae82a5b25fe0f74a936dd5e50be8439e0579f
Mateusz Rajek <mateusz_rajek@wp.pl>
2021-05-13 15:44:42 +0100

    Added user name, password and password repeating validation

--- src/Components/RegistrationView/Steps/Password.js
+++ src/Components/RegistrationView/Steps/Password.js
@@ -6,7 +6,7 @@ import { useForm } from 'react-hook-form';
 import Button from '../../Button/Button';
 
 function Password({ onSubmit }) {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, formState: { errors }, getValues } = useForm()
 
   return (
     <>
@@ -14,30 +14,53 @@ function Password({ onSubmit }) {
         <h1 className='registration__form__title'>Create an account</h1>
         <Input 
           className='registration__form__input' 
-          register={register('userName', {minLength: 2})}  
+          register={register('userName', {required: true, minLength: 4})}  
           text='User Name' 
           id='userName' 
           type='text'
           icon={person} 
         />
+        {errors.userName && errors.userName.type === 'required' && 
+        <p className='registration__form__error-message'>User Name is required</p>}
+        {errors.userName && errors.userName.type === 'minLength' && 
+        <p className='registration__form__error-message'>User Name requires at least 4 characters</p>}
         <Input 
           className='registration__form__input' 
-          register={register('password', {minLength: 2})}  
+          register={register('password', {required: true, minLength: 8, 
+            validate: value => [/[a-z]/, /[A-Z]/, /[0-9]/].every(pattern => pattern.test(value))})}  
           text='Password' 
           id='password' 
           type='password'
           icon={lock} 
           autocomplete='new-password' 
         />
+         {errors.userName && errors.userName.type === 'required' && 
+        <p className='registration__form__error-message'>Password is required</p>}
+        {errors.userName && errors.userName.type === 'minLength' && 
+        <p className='registration__form__error-message'>Password requires at least 8 characters</p>}
+        {errors.password && errors.password.type === 'validate' && 
+        <p className='registration__form__error-message'>Password must includes min 8 signs, at least one big letter and one number</p>}
         <Input 
           className='registration__form__input' 
-          register={register('passwordRep', {minLength: {value: 2}})} 
+          register={register('passwordRep', {required: true, minLength: 8,
+            validate: value => {
+              if (value === getValues().password) {
+                return true;
+              }
+              return false;
+            }})} 
           text='Repeat Password' 
           id='passwordRep' 
           type='password' 
           icon={lock} 
           autocomplete='new-password' 
         />
+        {errors.userName && errors.userName.type === 'required' && 
+        <p className='registration__form__error-message'>Password is required</p>}
+        {errors.userName && errors.userName.type === 'minLength' && 
+        <p className='registration__form__error-message'>Password requires at least 8 characters</p>}
+        {errors.password && errors.password.type === 'validate' && 
+        <p className='registration__form__error-message'>Password does't match</p>}
         <div className='registration__form__buttons'>
           <Button text='Prev' size='small' color='aquamarine' />
           <Button text='Register' size='medium' color='steelblue' />

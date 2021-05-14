1152ade730cfe3a5b684fcdc59bf9a5126fb5669
Mateusz Rajek <mateusz_rajek@wp.pl>
2021-05-14 16:59:35 +0100

    Added final step that redirecting to login page after registration

---
+++ src/Components/RegistrationView/Steps/FinalStep.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import Button from '../../Button/Button';
+
+function FinalStep({ goToLogin }) {
+    const [counter, setCounter] = useState(5);
+
+    const counterSetUp = () => {
+        const timer =
+          counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+          counter === 0 && goToLogin();
+        return () => clearInterval(timer);
+      };
+    
+      useEffect(counterSetUp, [counter, goToLogin]);
+
+  return (
+    <div className='registration__container__form registration__container__form--rounded'>
+        <p>Your accout has been registered.</p> 
+        <p>Redirecting to Login <span className={'registration__container__form__counter'}>{counter}</span></p> 
+        <p>or</p>
+        <Button text='Sign in' size='small' color='lightcollar' onClick={() => goToLogin()}/>
+    </div>
+  );
+}
+
+export default FinalStep;

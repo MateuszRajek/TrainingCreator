2e5b49001ec7be5624b581623b22ac2ded990e0f
Mateusz Rajek <mateusz_rajek@wp.pl>
2021-05-26 13:23:46 +0100

    Added visible/non-visible icons to password input; Implemented visibility toggle on icon click

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
+import eye from '../../assets/eye.png';
+import eyeInvisible from '../../assets/eye-invisible.png';
 import './_Input.sass';
 
 function Input({ id, type, register, text, className, icon, autocomplete, value }) {
   const [inputValue] = useState(value)
+  const [isVisible, setVisibility] = useState(false)
+  const [inputType, setInputType] = useState(type)
+
+  const togglePasswordVisibility = () => {
+    setVisibility(!isVisible)
+    setInputType(inputType === 'password' ? 'text' : 'password')
+  }
 
   return (
     <>
@@ -10,11 +19,12 @@ function Input({ id, type, register, text, className, icon, autocomplete, value 
       <div className='input__wrapper'>
         {icon && <div className='input__icon'><img src={icon} alt={`${id}-icon`}/></div>}
         <input 
-          {...{ id, type, placeholder:text, ...register, className: `input ${className}`, 
+          {...{ id, type: 'password' ? inputType : type, placeholder:text, ...register, className: `input ${className}`, 
           autoComplete: autocomplete ? autocomplete : null, value:inputValue }} 
         />
+        {isVisible && type === 'password' && <div onClick={() => togglePasswordVisibility()} className='password-visibility__input__icon'><img src={eye} alt={`${id}-icon`}/></div>}
+        {!isVisible && type === 'password' && <div onClick={() => togglePasswordVisibility()} className='password-visibility__input__icon'><img src={eyeInvisible} alt={`${id}-icon`}/></div>}
       </div>
-      
     </>
   );
 }

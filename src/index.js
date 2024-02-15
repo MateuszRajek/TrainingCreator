81dcad36e0e416c2dfab6c2bc8d98186a80390cc
Mateusz Rajek <mateusz_rajek@wp.pl>
2024-02-15 18:31:48 +0100

    Updated router config

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { RouterProvider } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import RegistrationView from "./Components/RegistrationView/RegistrationView";
+import LoginView from "./Components/LoginView/LoginView";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/registration",
+    element: <RegistrationView />,
+  },
+  {
+    path: "/login",
+    element: <LoginView />,
+  },
+]);
+
+root.render(
   <React.StrictMode>
-    <RouterProvider>
+    <RouterProvider router={router}>
       <App />
     </RouterProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

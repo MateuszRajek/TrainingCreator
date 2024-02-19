dc24f57d9b6972bc7f63880e7ceb6b9d3abda540
Mateusz Rajek <mateusz_rajek@wp.pl>
2024-02-19 18:20:53 +0100

    Added screen navigation in dahsboard and handled displaying screen that has been choosen

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import CalendarView from "./Calendar/Calendar";
 import Chat from "./Chat/Chat";
 import Tasks from "./Tasks/Tasks";
 import Trainings from "./Trainings/Trainings";
+import Button from "../../components/Button/Button";
+
+const dashboardScreens = {
+  chat: Chat,
+  tasks: Tasks,
+  calendar: CalendarView,
+  trainings: Trainings,
+};
+
+const dashboardNav = [
+  {
+    screen: "chat",
+    title: "Chat",
+  },
+  {
+    screen: "tasks",
+    title: "Tasks",
+  },
+  {
+    screen: "calendar",
+    title: "Calendar",
+  },
+  {
+    screen: "trainings",
+    title: "Trainings",
+  },
+];
 
 function Dashboard() {
+  const [selectedScreen, setSelectedScreen] = useState("tasks");
+
+  const ActiveScreen = dashboardScreens[selectedScreen];
+
+  const handleNavClick = (screen) => {
+    setSelectedScreen(screen);
+  };
+
   return (
     <section className="main">
       <div className="container top">
-        <CalendarView />
-        <Tasks />
+        {dashboardNav.map(({ screen, title }) => (
+          <Button text={title} onClick={() => handleNavClick(screen)} />
+        ))}
       </div>
       <div className="container center">
-        <Trainings />
-        <Chat />
+        <ActiveScreen />
       </div>
     </section>
   );
